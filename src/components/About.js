308aed7fa30360ba765eb347e9b0e1d627001b8e
+ <+>
2025-04-21 16:34:01 +0300

    corrected files

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,11 @@
 import React from "react";
 import Links from "./Links";
 
-function About({ bio, github, linkedin }) {
+function About({ bio, links }) {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      {/* Conditionally render bio */}
-      {bio && bio.trim() && <p>{bio}</p>}
-
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-
-      {/* Pass github and linkedin props down to the Links component */}
-      <Links github={github} linkedin={linkedin} />
+      {bio && bio.trim() !== "" && <p>{bio}</p>}
+      <Links github={links?.github} linkedin={links?.linkedin} />
     </div>
   );
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -2,15 +2,18 @@ import React from "react";
 
 function Links({ github, linkedin }) {
   return (
-    <div>
+    <div className="links">
       <h3>Links</h3>
-      <a href={github} target="_blank" rel="noreferrer">
-        {github}
-      </a>
-      <br />
-      <a href={linkedin} target="_blank" rel="noreferrer">
-        {linkedin}
-      </a>
+      {github && (
+        <a href={github} target="_blank" rel="noopener noreferrer">
+          {github}
+        </a>
+      )}
+      {linkedin && (
+        <a href={linkedin} target="_blank" rel="noopener noreferrer">
+          {linkedin}
+        </a>
+      )}
     </div>
   );
 }
